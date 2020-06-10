f737229294b86f420ccb32832ab7d1b72bba10a3
Andrew Nashaat <andrewnashaat@yahoo.com>
2020-06-10 03:21:27 +0200

    add login and register xml

--- app/src/main/java/com/example/goldenhand/Register.java
+++ app/src/main/java/com/example/goldenhand/Register.java
@@ -2,12 +2,73 @@ package com.example.goldenhand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
-
+    public Spinner spinner ;
+    public android.widget.Switch Switch;
+    public EditText multiline ,  phone ,address ;
+    public TextView store ;
+    public CheckBox online , physical ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        spinner =  findViewById(R.id.myspinner);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Register.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.egyptgov));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(myAdapter);
+
+
+        Switch=findViewById(R.id.switch1);
+        multiline=findViewById(R.id.multiline);
+        phone=findViewById(R.id.phone);
+        address=findViewById(R.id.address);
+        store=findViewById(R.id.store);
+        online=findViewById(R.id.onlinckbox);
+        physical=findViewById(R.id.physicalckbox);
+
+
+        Switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+                    multiline.setVisibility(View.VISIBLE);
+                    phone.setVisibility(View.VISIBLE);
+                    address.setVisibility(View.VISIBLE);
+                    store.setVisibility(View.VISIBLE);
+                    online.setVisibility(View.VISIBLE);
+                    physical.setVisibility(View.VISIBLE);
+                    spinner.setVisibility(View.VISIBLE);
+
+
+
+
+                }else {
+
+                    multiline.setVisibility(View.INVISIBLE);
+                    phone.setVisibility(View.INVISIBLE);
+                    address.setVisibility(View.INVISIBLE);
+                    store.setVisibility(View.INVISIBLE);
+                    online.setVisibility(View.INVISIBLE);
+                    physical.setVisibility(View.INVISIBLE);
+                    spinner.setVisibility(View.INVISIBLE);
+
+                }
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/goldenhand/login_page.java
@@ -0,0 +1,107 @@
+package com.example.goldenhand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.view.View;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class login_page extends AppCompatActivity {
+    EditText ed_email,ed_password;
+
+    String str_email,str_password;
+    String url = "https://arsltechmysql.000webhostapp.com/login.php";
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+
+            ed_email = findViewById(R.id.inputEmail);
+            ed_password = findViewById(R.id.inputPassword);
+
+        }
+
+    public void Login(View view) {
+
+        if(ed_email.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
+        }
+        else if(ed_password.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Please Wait..");
+
+            progressDialog.show();
+
+            str_email = ed_email.getText().toString().trim();
+            str_password = ed_password.getText().toString().trim();
+
+
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    progressDialog.dismiss();
+
+                    if(response.equalsIgnoreCase("logged in successfully")){
+
+                        ed_email.setText("");
+                        ed_password.setText("");
+                        startActivity(new Intent(getApplicationContext(),HomePage.class));
+                        Toast.makeText(login_page.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(login_page.this, response, Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            },new Response.ErrorListener(){
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.dismiss();
+                    Toast.makeText(login_page.this, error.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            ){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String, String>();
+                    params.put("email",str_email);
+                    params.put("password",str_password);
+                    return params;
+
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(login_page.this);
+            requestQueue.add(request);
+
+
+
+
+        }
+    }
+
+    public void moveToRegistration(View view) {
+        startActivity(new Intent(getApplicationContext(),Register.class));
+        finish();
+    }}
